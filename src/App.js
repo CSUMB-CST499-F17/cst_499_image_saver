3a13783763040f6106c662865fbc2b4ea131d0cb
Michael Wolff <mwolff@csumb.edu>
2017-09-20 18:01:05 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src="https://static1.squarespace.com/static/58519e11440243de222800ae/t/599f0765893fc0dfd27cc62f/1505171830890/?format=1500w" className="App-logo" alt="logo" />
+          <h2>Panafold</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
