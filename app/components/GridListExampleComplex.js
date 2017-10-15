1ae416498d3e6c1b0add5f4ba7aca4866446868a
Michael Wolff <mwolff@csumb.edu>
2017-10-15 06:15:09 +0000

    Gridlist is dynamically generated finally

--- app/components/GridListExampleComplex.js
+++ app/components/GridListExampleComplex.js
@@ -3,6 +3,9 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
+
+var background = chrome.extension.getBackgroundPage();
+
 const styles = {
   root: {
     display: 'flex',
@@ -18,34 +21,74 @@ const styles = {
 
 const tilesData = [
   {
-    //img: resultImgs[0],
-    img: 'https://i.redd.it/d3zti26pnmqz.jpg',
+    img: background.resultImgs[0],
+    //img: 'https://i.redd.it/d3zti26pnmqz.jpg',
     featured: true,
   },
   {
-    img: 'https://i.redd.it/ktpwf5ajauqz.jpg',
+    img: background.resultImgs[1],
   },
   {
-    img: 'https://i.redd.it/d3zti26pnmqz.jpg',
+    img: background.resultImgs[2],
   },
   {
-    img: 'https://i.redd.it/5jnc1tucrtqz.jpg',
-    featured: false,/*This means that the image is going to be larger than others*/
+    img: background.resultImgs[3],
+    featured: false,//This means that the image is going to be larger than others
   },
   {
-    img: 'https://i.imgur.com/k1uG7TY.jpg',
+    img: background.resultImgs[4],
   },
   {
-    img: 'https://i.imgur.com/yEqEsly.jpg',
+    img: background.resultImgs[5],
   },
   {
-    img: 'https://i.redd.it/3txbwlqzebqz.jpg',
+    img: background.resultImgs[6],
   },
   {
-    img: 'https://i.redd.it/lu6zvqf7ubqz.jpg',
+    img: background.resultImgs[7],
   },
+    {
+    img: background.resultImgs[8],
+  },
+    {
+    img: background.resultImgs[9],
+  },
+    {
+    img: background.resultImgs[10],
+  },
+    {
+    img: background.resultImgs[11],
+  },
+    {
+    img: background.resultImgs[12],
+  },
+    {
+    img: background.resultImgs[13],
+  },
+    {
+    img: background.resultImgs[14],
+  },
+    {
+    img: background.resultImgs[15],
+  },
+    {
+    img: background.resultImgs[16],
+  },
+    {
+    img: background.resultImgs[17],
+  },
+  
 ];
 
+
+
+//My array that I am going to populate with a flippin array :P
+/*
+const tilesData = [];
+for(i = 0; i <background.resultImgs.length;i++){
+  tilesData[i].img = background.resultImgs[i]
+}
+*/
 /**
  * This example demonstrates "featured" tiles, using the `rows` and `cols` props to adjust the size of the tile.
  * The tiles have a customised title, positioned at the top and with a custom gradient `titleBackground`.
