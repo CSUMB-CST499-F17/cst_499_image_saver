4ffb380a25926faf75ce58033e4eb53b4e64c6f4
Ulysses Vega <raider_aegis@yahoo.com>
2017-10-17 23:48:37 -0700

    Additional Codes

---
+++ file_downloader.js
@@ -0,0 +1,26 @@
+// JavaScript File
+
+//Download a file from a url.
+
+function saveFile(url){
+    
+    var filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
+    var xhr = new XMLHttpRequest();
+    
+    xhr.responseType = 'blob';
+    xhr.onload = function(){
+        
+        var a = document.createElement('a');
+        a.href = window.URL.createObjectURL(xhr.response); //xhr.response is a blob
+        a.download = filename;
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        //delete a;
+        
+    }; 
+    
+    xhr.open('GET', url);
+    xhr.send();
+    
+}
